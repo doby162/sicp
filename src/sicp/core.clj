b7e2e9fa8f7e5b340d6d7f27cd5e77df8edfea1b
Michael Dorian <doby162@gmail.com>
2019-12-24 10:16:00 -0600

    chapter one, newtons method

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -1,7 +1,38 @@
 (ns sicp.core
+  (:require [clojure.math.numeric-tower :as math])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn square
+  [x] (* x x))
+
+(defn good-enough?
+  [guess x]
+  (<
+   (math/abs (- (square guess) x))
+   0.000000000001))
+
+(defn average
+  [x y]
+  (/ (+ x y) 2))
+
+(defn improve
+  [guess x]
+  (average  guess (/ x guess)))
+
+(defn sqrt-iter
+  [guess x]
+  (cond
+    (good-enough? guess x)
+    guess
+    :else
+    (recur (improve guess x)
+           x)))
+
+(defn sqrt
+  [x]
+  (sqrt-iter 1.0 x))
