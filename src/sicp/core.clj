18e65149edb23b65166857faf0f1c6d2e9b32c68
Michael Dorian <doby162@gmail.com>
2019-12-24 11:00:06 -0600

    ex 1.8

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -10,10 +10,13 @@
 (defn square
   [x] (* x x))
 
+(defn cube
+  [x] (* x x x))
+
 (defn good-enough?
-  [guess x]
+  [guess x fun]
   (<
-   (math/abs (- (square guess) x))
+   (math/abs (- (fun guess) x))
    0.000000000001))
 
 (defn average
@@ -21,13 +24,25 @@
   (/ (+ x y) 2))
 
 (defn improve
-  [guess x]
-  (average  guess (/ x guess)))
+  "((y + (y / x))  / x)
+  y is the guess, x the input"
+  [y x]
+  (average  y (/ x y)))
+
+(defn improve-cube
+  "((x / y^2) + 2y) / 3
+  y is the guess, x the input"
+  [y x]
+  (/
+   (+
+    (* 2 y)
+    (/ x (* y y)))
+   3))
 
 (defn sqrt-iter
   [guess x]
   (cond
-    (good-enough? guess x)
+    (good-enough? guess x square)
     guess
     :else
     (recur (improve guess x)
@@ -36,3 +51,17 @@
 (defn sqrt
   [x]
   (sqrt-iter 1.0 x))
+
+(defn cube-root-iter
+  [guess x]
+  (cond
+    (good-enough? guess x cube)
+    guess
+    :else
+    (recur
+     (improve-cube guess x)
+     x)))
+
+(defn cube-root
+  [x]
+  (cube-root-iter 1.0 x))
